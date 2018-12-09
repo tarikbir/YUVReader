c0273ed32e4e57d6d9fa6c8d6e7db2933352ed04
Tarik Bir <7569835+tarikbir@users.noreply.github.com>
2018-12-09 15:35:51 +0300

    YUV ne diyirsen

--- YUVReader/MainWindow.xaml.cs
+++ YUVReader/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,39 @@ using System.Windows.Shapes;
 
 namespace YUVReader
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        string File;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "YUV files (*.yuv)|*.yuv";
+            openFileDialog.Title = "Select a valid YUV file...";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (openFileDialog.ShowDialog() != true) return;
+
+            if (OpenFile(openFileDialog.FileName))
+            {
+                lblNoFileSelectedError.Visibility = Visibility.Hidden;
+                mainGrid.Background.Opacity = 0.4;
+            }
+        }
+
+        private bool OpenFile(string fileName)
+        {
+            File = fileName;
+            return true;
+        }
+
+        private void MenuExit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
         }
     }
 }
