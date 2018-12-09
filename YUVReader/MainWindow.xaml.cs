9ebf54f414fe2a8b42da5a4a914b5a9473a70ba8
Tarik Bir <7569835+tarikbir@users.noreply.github.com>
2018-12-09 18:41:27 +0300

    YUV noliyi

--- YUVReader/MainWindow.xaml.cs
+++ YUVReader/MainWindow.xaml.cs
@@ -1,24 +1,15 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Accord.Video.FFMPEG;
+using YuvFormatter;
+using System.IO;
 
 namespace YUVReader
 {
     public partial class MainWindow : Window
     {
-        string File;
 
         public MainWindow()
         {
@@ -32,23 +23,55 @@ namespace YUVReader
             openFileDialog.Title = "Select a valid YUV file...";
             openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             if (openFileDialog.ShowDialog() != true) return;
+            lblNoFileSelectedError.Visibility = Visibility.Hidden;
+            mainGrid.Background.Opacity = 0.4;
+            btnReadFile.IsEnabled = true;
 
-            if (OpenFile(openFileDialog.FileName))
-            {
-                lblNoFileSelectedError.Visibility = Visibility.Hidden;
-                mainGrid.Background.Opacity = 0.4;
-            }
-        }
+            var bytes = File.ReadAllBytes(openFileDialog.FileName);
 
-        private bool OpenFile(string fileName)
-        {
-            File = fileName;
-            return true;
+            var bmp = YuvFormatter.YuvConverter.SourceFromYuv(bytes, 300, 300);
+
+            //mediaViewer.Source = openFileDialog.OpenFile();
         }
 
         private void MenuExit_Click(object sender, RoutedEventArgs e)
         {
             System.Windows.Application.Current.Shutdown();
+        }
+
+        private void MenuFormat_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (MenuItem item in menuFormat.Items)
+            {
+                if(sender != item)
+                {
+                    item.Unchecked -= MenuFormat_Unchecked;
+                    item.IsChecked = false;
+                    item.Unchecked += MenuFormat_Unchecked;
+                }
+            }
+            //seciliobje = (sender as MenuItem).Header.ToString();
+        }
+
+        private void MenuFormat_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var item = (sender as MenuItem);
+            item.Checked -= MenuFormat_Checked;
+            item.IsChecked = true;
+            item.Checked += MenuFormat_Checked;
+            e.Handled = true;
+        }
+
+        private void MenuSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = "*.bmp";
+            saveFileDialog.Filter = "Bitmap files (*.bmp)|*.bmp";
+            saveFileDialog.Title = "Select a path to save bitmap";
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (saveFileDialog.ShowDialog() != true) return;
+
+            //btmp.Save(saveFileDialog.FileName);
         }
     }
 }
